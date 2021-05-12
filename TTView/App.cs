faaf36142f8bca6e74a5041b021b184c40189b26
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2021-05-12 22:39:39 +0700

    Initial commit

---
+++ TTView/App.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TTView
+{
+    public class App
+    {
+        public static void Close() {
+            main.Close();
+        }
+
+        static bool __running = true;
+        static void _subcribeTask(IMain main)
+        {
+            var redis = new RedisBase(new RedisSetting(REDIS_TYPE.ONLY_SUBCRIBE, 1001));
+            redis.PSUBSCRIBE("*");
+            var bs = new List<byte>();
+            while (__running)
+            {
+                if (!redis.m_stream.DataAvailable)
+                {
+                    if (bs.Count > 0)
+                    {
+                        var data = redis.__getBodyPublish(bs.ToArray());
+                        bs.Clear();
+                        main._requestResponse(data);
+                    }
+                    Thread.Sleep(100);
+                    continue;
+                }
+                byte b = (byte)redis.m_stream.ReadByte();
+                bs.Add((byte)b);
+            }
+        }
+        static fMain main;
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            main = new fMain();
+            //var t = new Thread(new ParameterizedThreadStart((p) => _subcribeTask((IMain)p)));
+            //t.Start(main);
+            Application.Run(main);
+            __running = false;
+            //t.Abort();
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+    }
+
+    public interface IMain
+    {
+        void _requestResponse(Tuple<string, byte[]> data);
+    }
+
+}
---
+++ TTView/fMain.cs
@@ -0,0 +1,303 @@
+﻿using FarsiLibrary.Win;
+using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TTView
+{
+    public class fMain : Form, IMain
+    {
+        #region [ MAIN ]
+
+        FATabStrip m_tabs;
+        PictureBox m_image;
+
+        const bool m_hook_MouseMove = true;
+        private Panel m_resize;
+        private bool m_resizing = false;
+
+        public fMain()
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+            //this.BackColor = Color.Black;
+
+            this.KeyPreview = true;
+            this.KeyUp += main_keyUp;
+            this.Shown += main_initUI;
+        }
+
+        void main_initUI(object sender, EventArgs e)
+        {
+            this.Width = 800;
+            this.Height = 600;
+
+            m_tabs = new FATabStrip()
+            {
+                Dock = DockStyle.Fill,
+                //AlwaysShowClose = false
+                AlwaysShowMenuGlyph = false
+            };
+            this.Controls.Add(m_tabs);
+
+            m_image = new PictureBox()
+            {
+                BackColor = SystemColors.Info,
+                Top = 0,
+                Left = 0,
+                Width = this.Width + 400,
+                Height = 900
+            };
+            var box = new FlowLayoutPanel()
+            {
+                Dock = DockStyle.Fill,
+                BorderStyle = BorderStyle.None,
+                AutoScroll = true,
+            };
+            box.Controls.Add(m_image);
+
+            var m = new FATabStripItem("Main", box)
+            {
+                CanClose = false
+            };
+            m_tabs.Items.Add(m);
+
+            m_image.MouseMove += f_form_move_MouseDown;
+
+
+            // RESIZE
+            m_resize = new Panel()
+            {
+                Anchor = AnchorStyles.Bottom | AnchorStyles.Right,
+                BackColor = Color.Black,
+                Size = new Size(8, 8),
+                Top = this.Height - 8,
+                Left = this.Width - 8,
+            };
+            this.Controls.Add(m_resize);
+            m_resize.MouseDown += (se, ev) => { f_hook_mouse_Open(); m_resizing = true; };
+            m_resize.MouseUp += (se, ev) =>
+            {
+                m_resizing = false;
+                f_hook_mouse_Close();
+                //Debug.WriteLine("RESIZE: ok ");
+                //if (m_media.playState == WMPLib.WMPPlayState.wmppsPlaying)
+                //{
+                //    m_media.Ctlcontrols.pause();
+                //    m_media.Ctlcontrols.play();
+                //}
+                //else if (m_media.playState == WMPLib.WMPPlayState.wmppsPaused)
+                //{
+                //    m_media.Ctlcontrols.play();
+                //    m_media.Ctlcontrols.pause();
+                //}
+            };
+            m_resize.BringToFront();
+        }
+
+        void main_keyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyData)
+            {
+                case Keys.Right:
+                    //pageOpen(PageNumber + 1);
+                    break;
+                case Keys.Left:
+                    //pageOpen(PageNumber - 1);
+                    break;
+            }
+        }
+
+        /*////////////////////////////////////////////////////////////////////////*/
+
+        #region [ MOUSE MOVE: IN FORM, OUT FORM ]
+
+        private void f_mouse_move_intoForm(int x, int y)
+        {
+            f_form_Resize(x, y, MOUSE_XY.INT);
+        }
+
+        private void f_mouse_move_outForm(int x, int y)
+        {
+            f_form_Resize(x, y, MOUSE_XY.OUT);
+        }
+
+        #endregion
+
+        #region [ FORM MOVE, RESIZE ]
+
+        enum MOUSE_XY { OUT, INT };
+
+        void f_form_Resize(int x, int y, MOUSE_XY type)
+        {
+            if (m_resizing)
+            {
+                int max_x = this.Location.X + this.Width;
+                int max_y = this.Location.Y + this.Height;
+                this.Width = x - this.Location.X;
+                this.Height = y - this.Location.Y;
+            }
+        }
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void f_form_move_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        #endregion
+
+        #region [ HOOK MOUSE: MOVE, WHEEL ... ]
+
+        void f_hook_mouse_move_CallBack(MouseEventArgs e)
+        {
+            int max_x = this.Width + this.Location.X,
+                max_y = e.Location.Y + this.Height;
+            //Debug.WriteLine(this.Location.X + " " +e.X  + " " + max_x + " | " + this.Location.Y + " " +e.Y  + " " + max_y);
+
+            if (e.X > this.Location.X && e.X < max_x
+                && e.Y > this.Location.Y && e.Y < max_y)
+            {
+                //Debug.WriteLine("IN FORM: "+ this.Location.X + " " + e.X + " " + max_x + " | " + this.Location.Y + " " + e.Y + " " + max_y);
+                f_mouse_move_intoForm(e.X, e.Y);
+            }
+            else
+            {
+                //Debug.WriteLine("OUT FORM: " + this.Location.X + " " + e.X + " " + max_x + " | " + this.Location.Y + " " + e.Y + " " + max_y);
+                f_mouse_move_outForm(e.X, e.Y);
+            }
+        }
+
+        void f_hook_mouse_Open()
+        {
+            if (m_hook_MouseMove)
+                f_hook_mouse_SubscribeGlobal();
+        }
+
+        void f_hook_mouse_Close()
+        {
+            if (m_hook_MouseMove)
+                f_hook_mouse_Unsubscribe();
+        }
+
+        /*////////////////////////////////////////////////////////////////////////*/
+
+        private IKeyboardMouseEvents hook_events;
+
+        private void f_hook_mouse_SubscribeApplication()
+        {
+            f_hook_mouse_Unsubscribe();
+            f_hook_mouse_Subscribe(Hook.AppEvents());
+        }
+
+        private void f_hook_mouse_SubscribeGlobal()
+        {
+            f_hook_mouse_Unsubscribe();
+            f_hook_mouse_Subscribe(Hook.GlobalEvents());
+        }
+
+        private void f_hook_mouse_Subscribe(IKeyboardMouseEvents events)
+        {
+            hook_events = events;
+            //m_Events.KeyDown += OnKeyDown;
+            //m_Events.KeyUp += OnKeyUp;
+            //m_Events.KeyPress += HookManager_KeyPress;
+
+            //m_Events.MouseUp += OnMouseUp;
+            //m_Events.MouseClick += OnMouseClick;
+            //m_Events.MouseDoubleClick += OnMouseDoubleClick;
+
+            hook_events.MouseMove += f_hook_mouse_HookManager_MouseMove;
+
+            //m_Events.MouseDragStarted += OnMouseDragStarted;
+            //m_Events.MouseDragFinished += OnMouseDragFinished;
+
+            //if (checkBoxSupressMouseWheel.Checked)
+            //m_Events.MouseWheelExt += f_hook_mouse_HookManager_MouseWheelExt;
+            //else
+            ////hook_events.MouseWheel += f_hook_mouse_HookManager_MouseWheel;
+
+            //if (checkBoxSuppressMouse.Checked)
+            //m_Events.MouseDownExt += HookManager_Supress;
+            //else
+            //m_Events.MouseDown += OnMouseDown;
+        }
+
+
+        private void f_hook_mouse_Unsubscribe()
+        {
+            if (hook_events == null) return;
+            //m_Events.KeyDown -= OnKeyDown;
+            //m_Events.KeyUp -= OnKeyUp;
+            //m_Events.KeyPress -= HookManager_KeyPress;
+
+            //m_Events.MouseUp -= OnMouseUp;
+            //m_Events.MouseClick -= OnMouseClick;
+            //m_Events.MouseDoubleClick -= OnMouseDoubleClick;
+
+            hook_events.MouseMove -= f_hook_mouse_HookManager_MouseMove;
+
+            //m_Events.MouseDragStarted -= OnMouseDragStarted;
+            //m_Events.MouseDragFinished -= OnMouseDragFinished;
+
+            //if (checkBoxSupressMouseWheel.Checked)
+            //m_Events.MouseWheelExt -= f_hook_mouse_HookManager_MouseWheelExt;
+            //else
+            //hook_events.MouseWheel -= f_hook_mouse_HookManager_MouseWheel;
+
+            //if (checkBoxSuppressMouse.Checked)
+            //m_Events.MouseDownExt -= HookManager_Supress;
+            //else
+            //m_Events.MouseDown -= OnMouseDown;
+
+            hook_events.Dispose();
+            hook_events = null;
+        }
+
+        private void f_hook_mouse_HookManager_MouseMove(object sender, MouseEventArgs e)
+        {
+            f_hook_mouse_move_CallBack(e);
+        }
+
+        ////private void f_hook_mouse_HookManager_MouseWheel(object sender, MouseEventArgs e)
+        ////{
+        ////    //Debug.WriteLine(string.Format("Wheel={0:000}", e.Delta));
+        ////    //f_hook_mouse_wheel_CallBack(e);
+        ////}
+
+        ////private void f_hook_mouse_HookManager_MouseWheelExt(object sender, MouseEventExtArgs e)
+        ////{
+        ////    //Debug.WriteLine(string.Format("Wheel={0:000}", e.Delta)); 
+        ////    //Debug.WriteLine("Mouse Wheel Move Suppressed.\n");
+        ////    e.Handled = true;
+        ////    //e.Handled = true; // true: break event at here, stop mouse wheel at here
+        ////}
+
+        /////////////////////////////////////////////////////////////
+
+
+        #endregion
+
+        /*////////////////////////////////////////////////////////////////////////*/
+
+        #endregion
+
+        public void _requestResponse(Tuple<string, byte[]> data)
+        {
+        }
+    }
+}
